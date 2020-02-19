3eda025a1a6706b84a53943c6c0d31502fb9a880
Andrzej Holub <aholubprof@gmail.com>
2020-02-19 22:52:18 +0300

    first

--- api/index.js
+++ api/index.js
@@ -1,27 +1,27 @@
 var router = require('express').Router();
 const nodemailer = require('nodemailer');
     
-let testEmailAccount = nodemailer.createTestAccount();
+// let testEmailAccount = nodemailer.createTestAccount();
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'asti.company1@gmail.com',
-        pass: 'asti2015074'
-    }
-});
+// let transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'asti.company1@gmail.com',
+//         pass: 'asti2015074'
+//     }
+// });
 
 
 router.post('/mail', function (req, res, next) {
     res.json(1);
-    var body = req.body;
-    let result = transporter.sendMail({
-        from: '"Форма сайта" <asti.company1@gmail.com>',
-        to: "sancej176@mail.ru",
-        subject: "Асти компани",
-        text: "This message was sent from Node js server.",
-        html: "Новый пользователь: " + body.name + ", номер телефона: " + body.phone
-    });
+    // var body = req.body;
+    // let result = transporter.sendMail({
+    //     from: '"Форма сайта" <asti.company1@gmail.com>',
+    //     to: "sancej176@mail.ru",
+    //     subject: "Асти компани",
+    //     text: "This message was sent from Node js server.",
+    //     html: "Новый пользователь: " + body.name + ", номер телефона: " + body.phone
+    // });
 });
   
 
