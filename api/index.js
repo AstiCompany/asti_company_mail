bdc2fc7ae8ac2b7635c9f0d82ae8b786bf6e13f1
Andrzej Holub <aholubprof@gmail.com>
2020-05-28 16:55:28 +0300

    Change e-mail

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ router.post('/mail', function (req, res, next) {
     var body = req.body;
     let result = transporter.sendMail({
         from: '"Форма сайта" <asti.company1@gmail.com>',
-        to: "sam-web.by@mail.ru",
+        to: "office@asti.by",
         subject: "Асти компани",
         text: "This message was sent from Node js server.",
         html: "Новый пользователь: " + body.name + ", номер телефона: " + body.phone + ", email: " + body.email + ", текст вопроса: " + body.message
